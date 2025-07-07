7d513e109adaf374dd1b70dcc0b3b5454632154f
Nishan <girinishan1234567890@gmail.com>
2025-07-07 15:10:31 +0545

    Refactor Firebase Messaging integration: remove unused code and improve notification handling

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:swamper_solution/consts/custom_text_styles.dart';
 import 'package:swamper_solution/firebase_options.dart';
 import 'package:swamper_solution/routes/app_route_config.dart';
@@ -26,10 +25,9 @@ void main() async {
 
 Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
   await Firebase.initializeApp();
-  print('Background message received: ${message.notification?.title}');
   NotificiationServices.showNotification(
-    title: message.notification?.title ?? 'Notification',
-    body: message.notification?.body ?? '',
+    title: message.notification!.title.toString(),
+    body: message.notification!.body.toString(),
   );
 }
 
@@ -47,8 +45,6 @@ class _MyAppState extends ConsumerState<MyApp> {
   void initState() {
     super.initState();
     _appRoute = AppRouteConfig();
-    // Set up FCM
-    _setupFCM();
 
     FirebaseAuth.instance.authStateChanges().listen((user) {
       if (mounted) {
@@ -57,51 +53,11 @@ class _MyAppState extends ConsumerState<MyApp> {
     });
 
     FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-      print('Foreground message received: ${message.notification?.title}');
       NotificiationServices.showNotification(
         title: message.notification?.title ?? 'Notification',
         body: message.notification?.body ?? '',
       );
     });
-  }
-
-  Future<void> _setupFCM() async {
-    try {
-      // Get FCM token
-      String? token = await FirebaseMessaging.instance.getToken();
-      print('FCM Token: $token');
-      if (token != null) {
-        // Save token to user's profile when user is authenticated
-        FirebaseAuth.instance.authStateChanges().listen((user) async {
-          if (user != null) {
-            await _saveFCMToken(user.uid, token);
-          }
-        });
-      }
-
-      // Listen for token refresh
-      FirebaseMessaging.instance.onTokenRefresh.listen((newToken) {
-        print('FCM Token refreshed: $newToken');
-        User? user = FirebaseAuth.instance.currentUser;
-        if (user != null) {
-          _saveFCMToken(user.uid, newToken);
-        }
-      });
-    } catch (e) {
-      print('Error setting up FCM: $e');
-    }
-  }
-
-  Future<void> _saveFCMToken(String userId, String token) async {
-    try {
-      await FirebaseFirestore.instance
-          .collection('profiles')
-          .doc(userId)
-          .update({'fcmToken': token});
-      print('FCM token saved for user: $userId');
-    } catch (e) {
-      print('Error saving FCM token: $e');
-    }
   }
 
   @override
